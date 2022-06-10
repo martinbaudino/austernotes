576e547fa8fc38e22b2163788a211297f0a1f387
Martín Baudino <martinbaudino@gmail.com>
2022-06-10 17:01:55 -0300

    Step 2.1: Check mail verification

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'package:austernotes/views/login_view.dart';
 import 'package:austernotes/views/register_view.dart';
+import 'package:austernotes/views/verify_email_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -37,7 +39,17 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              // final user = FirebaseAuth.instance.currentUser;
+              final user = FirebaseAuth.instance.currentUser;
+              if (user != null){
+                if(user.emailVerified){
+                  print('Email is verified!');
+                } else {
+                  return const VerifyEmailView();
+                }
+              } else {
+                return const LoginView();
+              }
+              return const Text('Done');
               // //final emailVerified = user?.emailVerified ?? false;
               // if (user?.emailVerified ?? false) {
               //   return const Text('Done');
