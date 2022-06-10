27c27696acfebb444068b758b4788f2b839bba65
Martín Baudino <martinbaudino@gmail.com>
2022-06-10 20:45:37 -0300

    Step 3: Add navigation between routes

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,12 @@ import 'package:austernotes/views/verify_email_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 import 'firebase_options.dart';
 
 void main() {
+  devtools.log('Init...');
   WidgetsFlutterBinding
       .ensureInitialized(); // Ensure Flutter bindings before configuring Firebase
   runApp(
@@ -33,37 +35,102 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user != null){
-                if(user.emailVerified){
-                  print('Email is verified!');
-                } else {
-                  return const VerifyEmailView();
-                }
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                return const NotesView();
               } else {
-                return const LoginView();
+                return const VerifyEmailView();
               }
-              return const Text('Done');
-              // //final emailVerified = user?.emailVerified ?? false;
-              // if (user?.emailVerified ?? false) {
-              //   return const Text('Done');
-              // } else {
-              //   return const VerifyEmailView();
-              // }
+            } else {
               return const LoginView();
+            }
 
-            default:
-              return const CircularProgressIndicator();
-          }
-        },
-      );
+          // //final emailVerified = user?.emailVerified ?? false;
+          // if (user?.emailVerified ?? false) {
+          //   return const Text('Done');
+          // } else {
+          //   return const VerifyEmailView();
+          // }
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
   }
 }
 
+enum Menuaction { logout }
 
+class NotesView extends StatefulWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AusterNotes MAIN'),
+        actions: [
+          PopupMenuButton<Menuaction>(
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<Menuaction>(
+                    value: Menuaction.logout, child: Text('Logout')),
+              ];
+            },
+            onSelected: (value) async {
+              //devtools.log(value.toString());
+              switch(value){
+                case Menuaction.logout:
+                  final shoulLogout = await showLogOutDialog(context);
+                  //devtools.log(shoulLogout.toString());
+                  if (shoulLogout){
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil('/login/', (_) => false);
+                  }
+                  break;
+              }
+            },
+          )
+        ],
+      ),
+      body: const Text('Hello AusterNotes!'),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sign out'),
+        content: const Text('Are you sure to sign out?'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Cancel')),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Log out')),
+        ],
+      );
+    },
+  ).then(
+      (value) => value ?? false); // It either returns the bool value or false
+}
